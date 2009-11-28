eb7ad4e0a4851847fc2ed4f0dce1e13131f137aa
rob <rob@localhost>
2009-11-28 01:18:56 -0500

    Initial bit to make the xml parser.

--- dom.go
+++ dom.go
@@ -4,7 +4,11 @@ package dom
  * Implements a very small, very non-compliant subset of the DOM Core Level 2
  */
 
-/*
+import (
+  "strings";
+  "xml";
+)
+
 // DOM2: http://www.w3.org/TR/DOM-Level-2-Core/core.html#ID-1950641247
 type Node interface {
 	NodeName() string;
@@ -23,13 +27,20 @@ type Document interface {
 	Node;
 	DocumentElement() Element;
 }
-*/
-
-type Document struct {
-  
-}
 
 func ParseString(s string) (d *Document){
   d = new(Document);
+  r := strings.NewReader(s);
+  p := xml.NewParser(r);
+  _,_ = p.Token(); // todo: get the data token by token
   return d;
+}
+
+// doc is our internal implementation of the Document interface
+type doc struct {
+  
+}
+
+func (*doc) DocumentElement() (e Element) {
+  return;
 }
