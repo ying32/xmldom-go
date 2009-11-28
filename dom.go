002ec31016fd1f907c83110c24d61e42d2ab0900
Jeff Schiller <codedread@gmail.com>
2009-11-27 22:51:40 -0600

    Starter interfaces for the DOM.

--- dom.go
+++ dom.go
@@ -1,5 +1,30 @@
 package dom
 
+/*
+ * Implements a very small, very non-compliant subset of the DOM Core Level 2
+ */
+
+/*
+// DOM2: http://www.w3.org/TR/DOM-Level-2-Core/core.html#ID-1950641247
+type Node interface {
+	NodeName() string;
+}
+
+// DOM2: http://www.w3.org/TR/DOM-Level-2-Core/core.html#ID-745549614
+type Element interface {
+	Node;
+	TagName() string;
+	GetAttribute(name string) string;
+	SetAttribute(name string, value string);
+}
+
+// DOM2: http://www.w3.org/TR/DOM-Level-2-Core/core.html#i-Document
+type Document interface {
+	Node;
+	DocumentElement() Element;
+}
+*/
+
 type Document struct {
   
 }
