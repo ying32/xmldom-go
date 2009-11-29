81b41c9386d0678942c0ae00cab972ce466f4392
Jeff Schiller <codedread@gmail.com>
2009-11-28 20:09:35 -0600

    Added test to make sure documentElement returns an Element.  Added failed test to check the Element.nodeName value is correct.

--- dom_test.go
+++ dom_test.go
@@ -5,10 +5,29 @@ import (
   "xml/dom";
 )
 
+// Document.nodeName should be #document
+// see http://www.w3.org/TR/DOM-Level-3-Core/core.html#ID-1841493061
 func TestDocumentNodeName(t *testing.T) {
   var d = dom.ParseString("<foo></foo>");
   if (d.NodeName() != "#document") {
     t.Errorf("Document.nodeName != #document");
+  }
+}
+
+// Document.documentElement should return an object implementing Element
+func TestDocumentElementIsAnElement(t *testing.T) {
+  var d = dom.ParseString("<foo></foo>");
+  _,ok := (d.DocumentElement()).(dom.Element);
+  if (!ok) {
+  	t.Errorf("Document.documentElement did not return an Element");
+  }
+}
+
+func TestDocumentElementNodeName(t *testing.T) {
+  var d = dom.ParseString("<foo></foo>");
+  root,_ := (d.DocumentElement()).(dom.Element);
+  if (root.NodeName() != "foo") {
+  	t.Errorf("Element.nodeName not set correctly");
   }
 }
 
