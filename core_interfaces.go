b2ff6a927c67430a9906a6643d587538763f5ec9
Jeff Schiller <codedread@gmail.com>
2009-12-01 07:54:27 -0600

    Fix compilation errors.

--- core_interfaces.go
+++ core_interfaces.go
@@ -3,7 +3,7 @@ package dom
 /*
  * Part of the xml/dom Go package
  *
- * Contains all the interfaces from DOM Core Level 3
+ * Declares the interfaces from DOM Core Level 3
  * http://www.w3.org/TR/DOM-Level-3-Core/
  *
  * Copyright (c) 2009, Rob Russell
@@ -14,7 +14,7 @@ package dom
 type Node interface {
 	NodeName() string;
 	NodeType() int;
-  AppendChild(child *Node) (*Node);
+	AppendChild(Node) Node;
 }
 
 // DOM3: http://www.w3.org/TR/DOM-Level-3-Core/core.html#ID-745549614
