bd0b2f6d99645e6a7c63113483f9610ccd38e2a6
Jeff Schiller <codedread@gmail.com>
2009-12-04 00:09:04 -0600

    Split out nodelist implementations to separate file and update Makefile

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ include $(GOROOT)/src/Make.$(GOARCH)
 TARG=xml/dom
 GOFILES=\
 	core_interfaces.go \
+	nodelists.go \
 	dom.go
 
 include $(GOROOT)/src/Make.pkg
--- dom.go
+++ dom.go
@@ -17,43 +17,6 @@ import (
 )
 
 // ====================================
-// NodeList implementation
-type _nodelist struct {
-}
-func (nl *_nodelist) Length() uint {
-  return 0;
-}
-func (nl *_nodelist) Item(index uint) Node {
-  return new(_node);
-}
-
-// A _childNodelist only stores a reference to its parent node.
-// This way the list can be live, each time Length() or Item is
-// called, fresh results are returned.
-type _childNodelist struct {
-  p *_node;
-}
-
-func (nl *_childNodelist) Length() uint {
-  return uint(nl.p.c.Len());
-}
-func (nl *_childNodelist) Item(index uint) Node {
-  n := Node(nil);
-  if index < uint(nl.p.c.Len()) {
-    // TODO: what if index == nl.p.c.Len() -1 and a node is deleted right now?
-    n = nl.p.c.At(int(index)).(Node);
-  }
-  return n;
-}
-func newChildNodelist(p *_node) (*_childNodelist) {
-  nl := new(_childNodelist);
-  nl.p = p;
-  return nl;
-}
-
-// ====================================
-
-// ====================================
 type _node struct {
   p Node; // parent
   c vector.Vector; // children
